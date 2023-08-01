43976ccd16625469e02c68be7d6f9c923c25b8e7
stesiam <52320488+stesiam@users.noreply.github.com>
2023-08-01 19:41:00 +0300

    Add w10 visualization

---
+++ 2023/w10/w10.R
@@ -0,0 +1,53 @@
+# Import libraries
+
+library(rnaturalearth) # ne_countries
+library(rnaturalearthdata) # medium scale option
+library(ggplot2)
+library(ggtext)
+
+library(osmdata)
+
+library(extrafont)
+library(sysfonts)
+
+library(readr)
+
+font_add_google(name = "Lilita One", family = "title", db_cache = F)
+font_add_google(name = "Ysabeau Office", family = "subtitle", db_cache = F)
+# Import dataset
+
+numbats <- read_csv("2023/w10/numbats.csv")
+
+# Set Plain Australia Map
+
+aus = ne_countries(country = "australia", 
+                   type = "countries", 
+                   scale = 'large',
+                   returnclass = "sf")
+
+# Plot numbats observations
+
+map = ggplot() +
+  geom_sf(data = aus) +
+  coord_sf(xlim = c(113, 155), ylim = c(-10, -46)) +
+  geom_point(data = numbats, aes(x = decimalLongitude, y = decimalLatitude, color = scientificName)) +
+  labs(
+    title = "<b>Numbats Observations</b>",
+    subtitle = paste0("In total ", nrow(numbats), " numbats have been observed in Australia. <br>
+                      The vast majority of those are <span style = 'color:red;'>Myrmecobius fasciatus</span>
+                      <br> and just 12 are <span style = 'color:dodgerblue;'>Myrmecobius fasciatus rufus</span>."),
+    caption = "stesiam, 2023"
+  ) +
+  theme_void() +
+  theme(
+    plot.title = element_markdown(family = "EB Garamond", size = 15),
+    plot.subtitle = element_markdown(family = "EB Garamond"),
+    plot.caption = element_markdown(family = "EB Garamond"),
+    legend.position = "none"
+  )
+
+ggsave(
+  filename = "2023/w10/w10-2023-tt.png",
+  plot = map,
+  device = "png",
+  bg = "white")
