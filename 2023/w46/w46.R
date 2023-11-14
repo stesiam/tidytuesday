57d0c43d91bbe460369851fdc516807d56db80e2
stesiam <52320488+stesiam@users.noreply.github.com>
2023-11-15 01:28:53 +0200

    Edit w46 visualization

--- 2023/w46/w46.R
+++ 2023/w46/w46.R
@@ -18,7 +18,8 @@ library(sysfonts)
 sysfonts::font_add_google("Oswald", "title")
 sysfonts::font_add_google("EB Garamond","eb")
 #sysfonts::font_add_google("Gentium Plus", "gp")
-sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
+sysfonts::font_add('gpr',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
+sysfonts::font_add('gpb',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Bold.ttf")
 
 sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
 sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
@@ -35,14 +36,13 @@ l = house %>%
   group_by(`Age Group`) %>%
   summarise(s = sum(Amount))
 
-l$label = "apple"
 
 title = glue("Diwali Sales Data")
 subtitle = glue("<b>Diwali</b>, (also called Deepavali) is the Hindu festival of lights with its variations also celebrated
-                in other <br>Indian religions. The celebrations generally last five or six days. On this week's dataset we have sales data.<br>
-                We observe that the age group 26-35 age group have spent the most money. On the other hand, the <br>underage people spent the less.
-                In total, the sales sums up to {round(sum(l$s)/1000000, 1)} millions rupes.")
-caption = glue("Tidy Tuesday, week 46<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2023")
+                in <br> other Indian religions. The celebrations generally last five or six days. In this week's dataset <br> we observe that the age group 26-35 
+                has spent the most money while underage individuals<br>spent the least.
+                In total, the sales sum up to <span style='font-weight: bold; font-family:gpb;'>{round(sum(l$s)/1000000, 1)} millions rupees</span>.")
+caption = glue("<span style='font-family:title; color:grey;'><span style='font-weight: bold;'>NOTE:</span> Each </span> <span style='font-family:fs; color:green;'>&#xf53a;</span> <span style='font-family:title; color:grey;'>corresponds to 1 million (1,000,000) rupees</span><br> Tidy Tuesday, week 46<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2023")
 
 
 
@@ -87,10 +87,11 @@ p1 = ggplot(waf, aes(fill = states, values = n/1000000, label = la))+
     axis.ticks.y = element_blank(),
     plot.title = element_markdown(family = "title", color ="#f9e099", face = "bold", fill = "black",margin = margin(t = 5, b = 5), hjust = 0.5),
     plot.title.position = "plot",
-    plot.subtitle = element_markdown(family = "eb",
+    plot.subtitle = element_markdown(family = "gp",
                                      color = "white",
                                      margin = margin(t = 5, b = 10, l = 10, r = 10),
-                                     fill = "black"),
+                                     fill = "black",
+                                     lineheight = 1.1),
     legend.background = element_rect(fill = "white"),
     legend.position = "none",
     legend.text = element_text(hjust = 0.5),
@@ -99,7 +100,7 @@ p1 = ggplot(waf, aes(fill = states, values = n/1000000, label = la))+
     plot.caption = element_markdown(family = "title", margin = margin(t = 5, r = 5), lineheight = 1.2,
                                     color = "white"),
     strip.background = element_rect(fill = "black"),
-    strip.text = element_text(color = "white",size = 15,face = "bold", family = "title")
+    strip.text = element_text(color = "white",size = 12,face = "bold", family = "title")
   )
 
 ggsave(
