5239f4e10dce4c33ae1d8addf97c1cd54e7f9622
stesiam <52320488+stesiam@users.noreply.github.com>
2023-11-21 20:13:05 +0200

    Edit w44 viz: Edit text labels to be shown in top of points

--- 2023/w44/w44.R
+++ 2023/w44/w44.R
@@ -14,6 +14,8 @@ library(sysfonts)
 
 sysfonts::font_add_google("Oswald", "title")
 sysfonts::font_add_google("EB Garamond","eb")
+sysfonts::font_add_google("Cabin Condensed","cc")
+
 #sysfonts::font_add_google("Gentium Plus", "gp")
 sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
 
@@ -45,7 +47,7 @@ e = horror_articles %>%
 g = left_join(d, e, by = c("author")) %>%
   filter(rating == "false")
 
-title = glue("<span style='font-family:fs; color: black;'>&#xf002;</span> Snopes outcomes per author")
+title = glue("<span style='font-family:fs; color: grey;'>&#xf002;</span> Snopes outcomes per author <span style='font-family:fs; color: grey;'>&#xf002;</span>")
 subtitle = glue("<b>Snopes</b>, formerly known as the Urban Legends Reference Pages, is a fact-checking website.
                 It has been <br>described as a 'well-regarded reference for sorting out myths and rumors' on the Internet.
                 The site <br>has also been seen as a source for both validating and debunking urban legends and similar stories<br> in American popular culture.
@@ -58,7 +60,7 @@ p1 <- g %>%
   arrange(-pct) %>%
   mutate(author=factor(author, levels=author)) %>%
   ggplot(., aes(x = pct, y = author)) +
-  geom_point(aes(color = "red"), size = 3) +
+  geom_point(color = "#a9180d", size = 14) +
   geom_segment(
     data = g %>%
       pivot_wider(
@@ -66,10 +68,14 @@ p1 <- g %>%
         values_from = pct
       ),
     aes(x = 0, xend = false, y = author, yend = author),
-    alpha = 0.8, color = "gray90", linewidth = 0.8
+    color = "#a9180d", linewidth = 0.2
   ) +
   scale_x_continuous(limits = c(0,1), expand = c(0, 0)) +
-  geom_text(aes(x = pct+0.05, y = author, label = paste0(round(pct*100,1), "%")), size = 3, family = "eb") + 
+  geom_text(aes(x = pct, y = author, label = paste0(round(pct*100,1), " %")), 
+            size = 4, 
+            family = "cc", 
+            color = "white",
+            fontface = "bold") + 
   labs(
     title = title,
     subtitle = subtitle,
