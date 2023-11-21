51da6fa61f35a14460bf76cd7d724be46e3244ae
stesiam <52320488+stesiam@users.noreply.github.com>
2023-11-21 23:05:26 +0200

    Scraping wikipedia table about land area of each US state

---
+++ 2023/w41/scrapeLandArea.R
@@ -0,0 +1,37 @@
+# To clean data
+library(tidyverse)
+library(lubridate)
+library(janitor)
+# To scrape data
+library(rvest)
+library(httr)
+library(polite)
+
+url = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
+
+url_bow <- polite::bow(url)
+url_bow
+
+
+ind_html <-
+  polite::scrape(url_bow) %>%  # scrape web page
+  rvest::html_nodes("table.wikitable") %>% # pull out specific table
+  rvest::html_table(fill = TRUE) 
+
+
+
+s = ind_html[[1]] %>%
+  row_to_names(1) %>%
+  clean_names() %>%
+  select(.,c(1,2, 10)) %>%
+  setNames(c("state", "state_abbrev", "LandArea"))
+
+
+s1 = ind_html[[2]] %>%
+  row_to_names(1) %>%
+  clean_names() %>%
+  select(.,c(1,2, 10)) %>%
+  setNames(c("state", "state_abbrev", "LandArea"))
+
+
+s2 = bind_rows(s,s1)
