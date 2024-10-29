9f00cefd137bbcc0d68bc4700c1b4e0030aac0ae
stesiam <52320488+stesiam@users.noreply.github.com>
2024-10-29 16:33:06 +0200

    Update tt-w29 dataviz

--- 2023/w29/w29-el.R
+++ 2023/w29/w29-el.R
@@ -25,7 +25,7 @@ sysfonts::font_add_google("Jost","jost")
 
 
 #sysfonts::font_add_google("Gentium Plus", "gp")
-#sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
+sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
 
 sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
 sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
@@ -42,7 +42,7 @@ detectors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/
 df <- detectors %>%
   select(kind, .pred_class, model) %>%
   mutate(s = case_when(kind == .pred_class ~ "Yes",
-                           kind != .pred_class ~ "No")) %>%
+                       kind != .pred_class ~ "No")) %>%
   filter(model != "Human") %>%
   select(model, s) %>%
   group_by(model, s) %>%
@@ -52,13 +52,13 @@ df <- detectors %>%
   mutate(pct = round(count/countT *100, digits = 2)) %>%
   filter(s == "No")
 
-title = glue("<span style='font-family:fs; color:#ffffff;'>&#xf544;</span> Comparing Detectability of GPT <span style='font-family:fs; color:#ffffff;'>&#xf183;</span>")
-subtitle = glue("It is observed a big difference between GPT-3 and GPT-4 in terms
-    of detectability of AI response. The GPT-4 responses were 
-    <span style = 'color: #FFDAB9; font-weight:bold;'>{round((df$pct[[2]]- df$pct[[1]]), digits = 1)}</span>
-                % less possible to be detected by an AI detector scanner. Also,
-                <b>OriginalityAI</b> seems to have the higher accuracy among other
-                detectors.")
+title = glue("<span style='font-family:fs; color:#ffffff;'>&#xf544;</span> Τεχνητή Νοημοσύνη ή Άνθρωπος <span style='font-family:fs; color:#ffffff;'>&#xf183;</span>")
+subtitle = glue("Μία από τις χρήσης της τεχνητής νοημοσύνης (ΤΝ) είναι η παραγωγή κειμένου. Παρατηρείται ότι τα μοντέλα GPT-3 και GPT-4 της OpenAI παράγουν
+                  απαντήσεις που δεν μπορούν να αναγνωριστούν ως ΤΝ. Ειδικά για το GPT-4, οι απαντήσεις του είναι 
+    **<span style = 'color: #FFDAB9; font-weight:bold;'>{round((df$pct[[2]]- df$pct[[1]]), digits = 1)}</span>**% 
+    λιγόυερο πιθανό να αναγωνριστούν σε σύγκριση με το προηγούμενο μοντέλο, GPT-3. Όσον αφορά τα σκάνερ ταξινόμησης κειμένων,
+                η <b>OriginalityAI</b> έχει τη μεγαλύτερη ακρίβεια, ωστόσο είναι αρκετά μικρή. Συνεπώς, δεν έχουμε τα εργαλεία
+                που θα μας υποδεικνύουν με σχετική ακρίβεια αν ένα κείμενο το συνέγγραψε ένας άνθρωπος ή ΤΝ")
 caption = "Tidy Tuesday, week 29<br><span style='font-family:fb;'>&#xf09b;</span> stesiam, 2023"
 
 labels = c(
@@ -96,18 +96,18 @@ p1 = ggplot(df) +
   geom_point(x = 6, y = 70, color = "#C74B4B", size = 37) +
   geom_point(x = 2, y = 45, color = "#FF6F61", size = 29) +
   scale_size_continuous(range = c(29, 50)) +
-  geom_richtext(x = 2, y = 45, label = glue("**<span style='color:black;size:12pt'>GPT-3</span>**<br> {paste0(round(df$pct[1], digits = 0), ' %')}"),
-            aes(size = pct), 
-            family = "arvo",
-            color = "white",size = 6,
-            fill = NA, label.color = NA, hjust = 0.5) +
+  geom_richtext(x = 2, y = 45, label = glue("**<span style='color:black;font-family:arvo;'>GPT-3</span>**<br> {paste0(round(df$pct[1], digits = 0), ' %')}"),
+                aes(size = pct), 
+                family = "arvo",
+                color = "white",size = 6,
+                fill = NA, label.color = NA, hjust = 0.5) +
   geom_richtext(x = 6, y = 70, 
-                label = glue("**<span style='color:black;'>GPT-4</span>**<br> {paste0(round(df$pct[2], digits = 0), ' %')}"), aes(size = pct), 
-            family = "arvo",
-            color = "white",size = 8,
-            fill = NA, label.color = NA, hjust = 0.5) +
+                label = glue("**<span style='color:black;font-family:arvo;'>GPT-4</span>**<br> {paste0(round(df$pct[2], digits = 0), ' %')}"), aes(size = pct), 
+                family = "arvo",
+                color = "white",size = 8,
+                fill = NA, label.color = NA, hjust = 0.5) +
   geom_curve(
-    aes(x = 4, y = 83, xend = 1.5, yend = 80),
+    aes(x = 4.3, y = 84, xend = 1.5, yend = 80),
     color = "#C74B4B",
     curvature = 0.4,
     size = 0.4,
@@ -119,12 +119,12 @@ p1 = ggplot(df) +
   ) +
   geom_richtext(
     x = 1.6, y = 75, fill = NA, label.color = NA,
-    label = glue("Released in<br>March, 2023"),
-    size = 3, family = "jost",
+    label = glue("Κυκλοφόρησε τον<br>Μάρτιο του 2023"),
+    size = 3, family = "caption",
     color = "#E0E0E0", hjust = 0.5
   ) +
   geom_curve(
-    aes(x = 3, y = 35, xend = 5.5, yend = 40),
+    aes(x = 3, y = 35, xend = 5.5, yend = 35),
     color = "#FF6F61",
     curvature = 0.4,
     size = 0.4,
@@ -135,16 +135,16 @@ p1 = ggplot(df) +
     )
   ) +
   geom_richtext(
-    x = 5.8, y = 45, fill = NA, label.color = NA,
-    label = glue("Released in<br>June, 2020"),
-    size = 3, family = "jost",
+    x = 5.8, y = 40, fill = NA, label.color = NA,
+    label = glue("Κυκλοφόρησε τον<br>Ιούνιο του 2020"),
+    size = 3, family = "caption",
     color = "#E0E0E0", hjust = 0.5
   ) +
   scale_x_continuous(limits = c(0, 10)) +
   scale_y_continuous(expand = c(0, 0), limits = c(30,90)) +
   labs(
-    title = "Deceive AI Detectors by model",
-    subtitle = "(%) of texts generated from AI and not spotted",
+    title = "Ξεγελώντας τους ανιχνευτές ΤΝ",
+    subtitle = "(%) κειμένων που παράχθηκαν από ΤΝ και δεν εντοπίστηκαν",
     x = "Model",
     y = "Percentage"
   ) +
@@ -152,7 +152,7 @@ p1 = ggplot(df) +
   theme(
     legend.position = "none",
     axis.title = element_blank(),
-    plot.title = element_markdown(family = "jost",face = "bold", color = "black"),
+    plot.title = element_markdown(family = "serif",face = "bold", color = "black"),
     plot.subtitle = element_markdown(family = "caption", color = "#E0E0E0"),
     panel.background = element_rect(fill = "transparent", color = "transparent"),
     plot.background = element_rect(fill = "transparent", color = "transparent"),
@@ -182,20 +182,20 @@ p2 = detectors %>%
   geom_text(aes(x = pct-6, y = reorder(detector, pct), label = paste0(pct, " %")),
             family = "arvo", color = "white") +
   geom_text(aes(x = 1, y = reorder(detector, pct), label = detector),
-              family = "arvo", color = "black", hjust = 0, size = 3) +
+            family = "arvo", color = "black", hjust = 0, size = 3) +
   scale_x_continuous(expand = c(0,0), breaks = c(0, 20,40,60),
                      limits = c(0, 62)) +
   labs(
     x = "Accuracy",
     y = "",
-    title = "Accuracy of AI classification",
-    subtitle = "(%) TP + TN out of Total Predictions"
-    ) +
+    title = "Ακρίβεια ταξινόμησης",
+    subtitle = "(%) Αληθώς θετικά (ΤΝ) + Αληθώς Αρνητικά, επί του συνόλου"
+  ) +
   theme_classic(base_size = 8) +
   theme(
     legend.position = "none",
     axis.title = element_blank(),
-    plot.title = element_markdown(family = "jost",face = "bold", color = "black"),
+    plot.title = element_markdown(family = "serif",face = "bold", color = "black"),
     plot.subtitle = element_markdown(family = "caption", color = "#E0E0E0"),
     panel.background = element_rect(fill = "transparent", color = "transparent"),
     plot.background = element_rect(fill = "transparent", color = "transparent"),
@@ -211,13 +211,13 @@ bg_gradient <- grid::linearGradient(colours = rev(MetBrewer::met.brewer("Hokusai
 
 title_theme = theme(
   plot.title = element_markdown(
-    family = "jost", 
+    family = "serif", 
     face = "bold",hjust = 0.5,
     color = "#FFF5E1",
     margin = margin(t = 5, b = 5)),
   plot.subtitle = element_textbox_simple(
-    family = "jost", lineheight = 1.3,
-    size = 9,
+    family = "serif", lineheight = 1.3,
+    size = 8,
     margin = margin(b = 10, t = 5),
     color = "#E0E0E0"),
   plot.caption = element_markdown(family = "jost", lineheight = 1.3, color = "#E0E0E0", size = 7),
@@ -234,10 +234,9 @@ combined = (p1 + p2) &
 
 
 ggsave(
-  filename = "2023/w29/w29-2023-tt.png",
+  filename = "2023/w29/w29-2023-tt-el.png",
   plot = combined,
   device = "png",
   height = 4,
   width = 6)
-
 
